75ea15342efdb36e8745e750ed0b681de414d0a7
Lorn <lorn.br@gmail.com>
2009-10-29 08:08:20 -0200

    Introdução feita, com pre-requisito

---
+++ beamer/ryuzaki_example/hash.pl
@@ -0,0 +1,9 @@
+my %idade = {
+    "raul_seixas" => -10000,
+    "arthur_dent" => 42
+}
+print $idade{'arthur_dent'};
+# adicionando itens ao hash
+$idade{"jeremias"}=27;
+# deletando itens
+delete($idade{'raul_seixas'});
